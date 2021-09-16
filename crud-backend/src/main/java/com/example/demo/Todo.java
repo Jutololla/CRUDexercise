2af630f319da4fa88f4f334ff53689c195c26b69
Jutololla <julianangulop@gmail.com>
2021-09-15 22:46:15 -0500

    Logica de backend y frontend creada

---
+++ crud-backend/src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private boolean isComplete;
+
+    public Todo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+}
